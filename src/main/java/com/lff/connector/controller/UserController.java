3d4ddd3cd9727aac5a86e406f7d811a3052dd961
Lucas Freitas <lucasfujfreitas07@gmail.com>
2026-09-03 18:17:01 -0300

    feat: updateUser endpoint

--- src/main/java/com/lff/connector/controller/UserController.java
+++ src/main/java/com/lff/connector/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,4 +49,10 @@ public class UserController {
         service.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    @PutMapping("{id}")
+    public UserResponse updateUser(@PathVariable Long id, @RequestBody @Valid UserRequest dto) {
+        return service.updateUser(id, dto);
+    }
+
 }
