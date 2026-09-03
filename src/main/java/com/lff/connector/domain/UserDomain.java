25bcf91cd00fc9bb16e5ce9e0986a853f498a047
Lucas Freitas <lucasfujfreitas07@gmail.com>
2026-09-03 17:40:14 -0300

    feat: UserDomain class

---
+++ src/main/java/com/lff/connector/domain/UserDomain.java
@@ -0,0 +1,44 @@
+package com.lff.connector.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "users")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
+@Builder
+public class UserDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "Ativo", nullable = false)
+    private Boolean active;
+}
