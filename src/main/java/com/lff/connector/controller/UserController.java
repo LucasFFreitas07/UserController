65a2e5dd1d5b7e88d13e8f1db6990bf041410af5
Lucas Freitas <lucasfujfreitas07@gmail.com>
2026-09-03 18:16:29 -0300

    feat: deleteUser endpoint

--- src/main/java/com/lff/connector/controller/UserController.java
+++ src/main/java/com/lff/connector/controller/UserController.java
@@ -4,20 +4,19 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lff.connector.domain.UserDomain;
 import com.lff.connector.dto.UserRequest;
 import com.lff.connector.dto.UserResponse;
 import com.lff.connector.service.UserService;
 
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
@@ -42,5 +41,11 @@ public class UserController {
     public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest dto) {
         UserResponse response = service.createUser(dto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
