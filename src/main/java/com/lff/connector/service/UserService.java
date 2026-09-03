ad8254daef570fcf12ddb5e155050395a0dc84ce
Lucas Freitas <lucasfujfreitas07@gmail.com>
2026-09-03 19:02:14 -0300

    feat: - Added: PasswordEncoder - Removed: validateFields function update: deleteUser function now uses login '{id}'

--- src/main/java/com/lff/connector/service/UserService.java
+++ src/main/java/com/lff/connector/service/UserService.java
@@ -2,13 +2,12 @@ package com.lff.connector.service;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import com.lff.connector.domain.UserDomain;
 import com.lff.connector.dto.UserRequest;
 import com.lff.connector.dto.UserResponse;
-import com.lff.connector.handlers.BlankFieldException;
 import com.lff.connector.handlers.NotFoundException;
 import com.lff.connector.repository.UserRepository;
 
@@ -18,24 +17,11 @@ import jakarta.transaction.Transactional;
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
-    }
-
-    private void validateFields(UserRequest user) {
-        if (!StringUtils.hasText(user.first_name())) {
-            throw new BlankFieldException("Primeiro nome não pode ser vazio");
-        }
-        if (!StringUtils.hasText(user.last_name())) {
-            throw new BlankFieldException("Segundo nome não pode ser vazio");
-        }
-        if (!StringUtils.hasText(user.login())) {
-            throw new BlankFieldException("Login não pode ser vazio");
-        }
-        if (!StringUtils.hasText(user.email())) {
-            throw new BlankFieldException("E-mail não pode ser vazio");
-        }
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserResponse getById(Long id) {
@@ -55,39 +41,41 @@ public class UserService {
     public void deleteUser(Long id) {
 
         if (!repository.existsById(id)) {
-            throw new NotFoundException(String.format("Usuário com login <%d> não encontrado", id));
+            throw new NotFoundException(String.format("Usuário com id '%d' não encontrado", id));
         }
         this.repository.deleteById(id);
     }
 
     public UserResponse createUser(UserRequest user) {
-        validateFields(user);
+        String encodedPassword = this.passwordEncoder.encode(user.password());
+        
         UserDomain domain = new UserDomain();
         domain.setFirstName(user.first_name());
         domain.setLastName(user.last_name());
         domain.setLogin(user.login());
+        domain.setPassword(encodedPassword);
         domain.setEmail(user.email());
         domain.setActive(false);
-
         repository.save(domain);
         return UserResponse.from(domain);
     }
 
     public UserResponse updateUser(Long id, UserRequest user) {
-        validateFields(user);
+        String encodedPassword = this.passwordEncoder.encode(user.password());
         UserDomain domain = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException(String.format("Usuário com id '%d' não encontrado", id)));
 
         domain.setFirstName(user.first_name());
         domain.setLastName(user.last_name());
         domain.setLogin(user.login());
+        domain.setPassword(encodedPassword);
         domain.setEmail(user.email());
 
         repository.save(domain);
         return UserResponse.from(domain);
     }
 
-        public UserResponse deactivateUser(Long id) {
+    public UserResponse deactivateUser(Long id) {
         UserDomain domain = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException(String.format("Usuário com id '%d' não encontrado", id)));
 
