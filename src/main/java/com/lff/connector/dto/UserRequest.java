676b0630abbd2bc273d1b9e7f013231b3e9d92af
Lucas Freitas <lucasfujfreitas07@gmail.com>
2026-09-03 18:58:14 -0300

    update: Removed error message

--- src/main/java/com/lff/connector/dto/UserRequest.java
+++ src/main/java/com/lff/connector/dto/UserRequest.java
@@ -2,14 +2,13 @@ package com.lff.connector.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserRequest(
-        @Size(max = 20) @NotBlank(message = "Primeiro nome não pode ser vazio") String first_name,
-        @Size(max = 20) @NotBlank(message = "Segundo nome não pode ser vazio") String last_name,
-        @Size(min=8, max = 8) @NotBlank(message = "Login não pode ser vazio") String login,
-        @NotBlank(message = "E-mail não pode ser vazio") String email) {
+        @Size(max = 20) String first_name,
+        @Size(max = 20) String last_name,
+        @Size(min=8, max = 8) String login,
+        String email) {
 
 }
