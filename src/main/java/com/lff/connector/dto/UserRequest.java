9e894d47cc0e82c307b00276d26b44da162d1493
Lucas Freitas <lucasfujfreitas07@gmail.com>
2026-09-03 18:59:09 -0300

    update: Added password to Request

--- src/main/java/com/lff/connector/dto/UserRequest.java
+++ src/main/java/com/lff/connector/dto/UserRequest.java
@@ -9,6 +9,7 @@ public record UserRequest(
         @Size(max = 20) String first_name,
         @Size(max = 20) String last_name,
         @Size(min=8, max = 8) String login,
+        @Size(max = 50) String password,
         String email) {
 
 }
