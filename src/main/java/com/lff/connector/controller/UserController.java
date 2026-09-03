98b83f630a0961d39fa0ccb05cf53e4cec8ba779
Lucas Freitas <lucasfujfreitas07@gmail.com>
2026-09-03 17:40:39 -0300

    feat: UserController class

---
+++ src/main/java/com/lff/connector/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.lff.connector.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lff.connector.domain.UserDomain;
+import com.lff.connector.dto.UserRequest;
+import com.lff.connector.dto.UserResponse;
+import com.lff.connector.service.UserService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<UserDomain> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("{id}")
+    public UserDomain getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest dto) {
+        UserResponse response = service.createUser(dto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
