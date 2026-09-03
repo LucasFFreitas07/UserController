5909c706870f18585f98024fd2356fb364e2a548
Lucas Freitas <lucasfujfreitas07@gmail.com>
2026-09-03 18:17:33 -0300

    feat: activateUser/deactivateUser endpoint

--- src/main/java/com/lff/connector/controller/UserController.java
+++ src/main/java/com/lff/connector/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -54,5 +55,14 @@ public class UserController {
     public UserResponse updateUser(@PathVariable Long id, @RequestBody @Valid UserRequest dto) {
         return service.updateUser(id, dto);
     }
+    
+    @PatchMapping("{id}/deactivate")
+    public UserResponse deactivateUser(@PathVariable Long id) {
+        return service.deactivateUser(id);
+    }
 
+    @PatchMapping("{id}/activate")
+    public UserResponse activateUser(@PathVariable Long id) {
+        return service.activateUser(id);
+    }
 }
